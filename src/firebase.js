43521ce06261596f1f6bbc2c3c3345af80434e72
deniz Ay <denizay@std.iyte.edu.tr>
2022-03-10 19:17:10 +0300

    added uploading file using firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,20 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
-  apiKey: "AIzaSyBfCPr_TxMJDaMWNuoyXry6oLEXBGPMmko",
-  authDomain: "todo-react-firebase-65554.firebaseapp.com",
-  projectId: "todo-react-firebase-65554",
-  storageBucket: "todo-react-firebase-65554.appspot.com",
-  messagingSenderId: "973283416008",
-  appId: "1:973283416008:web:a78b246aeeac42548212fd",
+  apiKey: "AIzaSyCui4BVLB1-ODkyntg8X2bU09TW_m5Fxe0",
+  authDomain: "storage-firebase-app-63447.firebaseapp.com",
+  projectId: "storage-firebase-app-63447",
+  storageBucket: "storage-firebase-app-63447.appspot.com",
+  messagingSenderId: "239104825659",
+  appId: "1:239104825659:web:b78cd75aeb779d45457f1e",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
-export { db };
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const storage = getStorage(firebaseApp);
+
+export { db, auth, provider, storage };
